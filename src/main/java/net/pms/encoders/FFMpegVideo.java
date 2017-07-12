UniversalMediaServer/UniversalMediaServer

- Partially reverted "Store the subs color in UMS.conf as the hexa string" (9bcba891a3299f498c00aed3b5abc2d966bd66ff). - Implemented SubtitleColor - Improved related logging

src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -230,7 +230,7 @@ public class FFMpegVideo extends Player {
 
 						// XXX (valib) If the font size is not acceptable it could be calculated better taking in to account the original video size. Unfortunately I don't know how to do that.
 						subsFilter.append(",Fontsize=").append((int) 15 * Double.parseDouble(configuration.getAssScale()));
-						subsFilter.append(",PrimaryColour=").append(SubtitleUtils.convertColourToASSColourString(configuration.getSubsColor()));
+						subsFilter.append(",PrimaryColour=").append(configuration.getSubsColor().getFFmpegHexValue());
 						subsFilter.append(",Outline=").append(configuration.getAssOutline());
 						subsFilter.append(",Shadow=").append(configuration.getAssShadow());
 						subsFilter.append(",MarginV=").append(configuration.getAssMargin());
@@ -431,16 +431,17 @@ public class FFMpegVideo extends Player {
 
 		// Give priority to the renderer's maximum bitrate setting over the user's setting
 		if (rendererMaxBitrates[0] > 0 && rendererMaxBitrates[0] < defaultMaxBitrates[0]) {
-			defaultMaxBitrates = rendererMaxBitrates;
 			LOGGER.trace(
-				"Using the video bitrate limit from the renderer config ({} Mb/s) " +
-				"which is lower than the one from the program settings ({} Mb/s)",
+				"Using video bitrate limit from {} configuration ({} Mb/s) because " +
+				"it is lower than the general configuration bitrate limit ({} Mb/s)",
+				params.mediaRenderer.getRendererName(),
 				rendererMaxBitrates[0],
 				defaultMaxBitrates[0]
 			);
+			defaultMaxBitrates = rendererMaxBitrates;
 		} else {
 			LOGGER.trace(
-				"Using the video bitrate limit from the program settings ({} Mb/s)",
+				"Using video bitrate limit from the general configuration ({} Mb/s)",
 				defaultMaxBitrates[0]
 			);
 		}

src/main/java/net/pms/util/SubtitleUtils.java
@@ -440,7 +440,7 @@ public class SubtitleUtils {
 
 								break;
 							case "PrimaryColour":
-								params[i] = convertColourToASSColourString(configuration.getSubsColor());
+								params[i] = configuration.getSubsColor().getASSHexValue();
 								break;
 							case "Outline":
 								params[i] = configuration.getAssOutline();
@@ -525,7 +525,7 @@ public class SubtitleUtils {
 				fontScaleY = Double.toString(100 * Double.parseDouble(configuration.getAssScale()));
 			}
 
-			String primaryColour = convertColourToASSColourString(configuration.getSubsColor());
+			String primaryColour = configuration.getSubsColor().getASSHexValue();
 			String outline = configuration.getAssOutline();
 			String shadow = configuration.getAssShadow();
 			outputString.append("Style: Default,Arial,").append("15").append(',').append(primaryColour).append(",&H000000FF,&H00000000,&H00000000,0,0,0,0,").append(fontScaleX).append(',').append(fontScaleY).append(",0,0,1,").append(outline).append(',').append(shadow);
@@ -611,19 +611,6 @@ public class SubtitleUtils {
 
 	public static void deleteSubs() {
 		FileUtils.deleteQuietly(new File(configuration.getDataFile(SUB_DIR)));
-	}
-
-	/**
-	 * Converts the standard Colour RGB integer presentation to the SSA/ASS string format which
-	 * is formatted as BGR (really stupid SSA/ASS implementation)
-	 *
-	 * @param colour the RGB color in the integer format
-	 * @return Converted color string in the ASS format
-	 */
-	public static String convertColourToASSColourString(String colour) {
-		StringBuilder outputString = new StringBuilder();
-		outputString.append("&H").append(colour.substring(6, 8)).append(colour.substring(4, 6)).append(colour.substring(2, 4));
-		return outputString.toString();
 	}
 
 	/**

DigitalMediaServer/DigitalMediaServer

src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -230,7 +230,7 @@ public class FFMpegVideo extends Player {
 
 						// XXX (valib) If the font size is not acceptable it could be calculated better taking in to account the original video size. Unfortunately I don't know how to do that.
 						subsFilter.append(",Fontsize=").append((int) 15 * Double.parseDouble(configuration.getAssScale()));
-						subsFilter.append(",PrimaryColour=").append(SubtitleUtils.convertColourToASSColourString(configuration.getSubsColor()));
+						subsFilter.append(",PrimaryColour=").append(configuration.getSubsColor().getASSv4StylesHexValue());
 						subsFilter.append(",Outline=").append(configuration.getAssOutline());
 						subsFilter.append(",Shadow=").append(configuration.getAssShadow());
 						subsFilter.append(",MarginV=").append(configuration.getAssMargin());
@@ -431,16 +431,17 @@ public class FFMpegVideo extends Player {
 
 		// Give priority to the renderer's maximum bitrate setting over the user's setting
 		if (rendererMaxBitrates[0] > 0 && rendererMaxBitrates[0] < defaultMaxBitrates[0]) {
-			defaultMaxBitrates = rendererMaxBitrates;
 			LOGGER.trace(
-				"Using the video bitrate limit from the renderer config ({} Mb/s) " +
-				"which is lower than the one from the program settings ({} Mb/s)",
+				"Using video bitrate limit from {} configuration ({} Mb/s) because " +
+				"it is lower than the general configuration bitrate limit ({} Mb/s)",
+				params.mediaRenderer.getRendererName(),
 				rendererMaxBitrates[0],
 				defaultMaxBitrates[0]
 			);
+			defaultMaxBitrates = rendererMaxBitrates;
 		} else {
 			LOGGER.trace(
-				"Using the video bitrate limit from the program settings ({} Mb/s)",
+				"Using video bitrate limit from the general configuration ({} Mb/s)",
 				defaultMaxBitrates[0]
 			);
 		}

src/main/java/net/pms/util/SubtitleUtils.java
@@ -440,7 +440,7 @@ public class SubtitleUtils {
 
 								break;
 							case "PrimaryColour":
-								params[i] = convertColourToASSColourString(configuration.getSubsColor());
+								params[i] = configuration.getSubsColor().getASSv4StylesHexValue();
 								break;
 							case "Outline":
 								params[i] = configuration.getAssOutline();
@@ -525,7 +525,7 @@ public class SubtitleUtils {
 				fontScaleY = Double.toString(100 * Double.parseDouble(configuration.getAssScale()));
 			}
 
-			String primaryColour = convertColourToASSColourString(configuration.getSubsColor());
+			String primaryColour = configuration.getSubsColor().getASSv4StylesHexValue();
 			String outline = configuration.getAssOutline();
 			String shadow = configuration.getAssShadow();
 			outputString.append("Style: Default,Arial,").append("15").append(',').append(primaryColour).append(",&H000000FF,&H00000000,&H00000000,0,0,0,0,").append(fontScaleX).append(',').append(fontScaleY).append(",0,0,1,").append(outline).append(',').append(shadow);
@@ -611,19 +611,6 @@ public class SubtitleUtils {
 
 	public static void deleteSubs() {
 		FileUtils.deleteQuietly(new File(configuration.getDataFile(SUB_DIR)));
-	}
-
-	/**
-	 * Converts the standard Colour RGB integer presentation to the SSA/ASS string format which
-	 * is formatted as BGR (really stupid SSA/ASS implementation)
-	 *
-	 * @param colour the RGB color in the integer format
-	 * @return Converted color string in the ASS format
-	 */
-	public static String convertColourToASSColourString(String colour) {
-		StringBuilder outputString = new StringBuilder();
-		outputString.append("&H").append(colour.substring(6, 8)).append(colour.substring(4, 6)).append(colour.substring(2, 4));
-		return outputString.toString();
 	}
 
 	/**