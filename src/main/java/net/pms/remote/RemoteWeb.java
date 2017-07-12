SanketSKasar/UniversalMediaServer

- Created class DLNAImageProfile representing compliant DLNA media format profiles for images - Created class DLNAThumbnail that holds some metadata about the thumbnail in addition to the raw bytes - Created class DLNAThumbnailInputStream that simply is a "InputStream version" of DLNAThumbnail - Upped database version to 8 and changed type of THUMB column in FILES so that a DLNAThumbnail object is stored instead of the raw bytes - Made audio file tag parsing parse MP2 as MP3 files - Made separate logging for audio tag parsing when the file format isn't supported - Created ImageMetaData class for holding metadata about images - Rewrote DLNAResource.appendThumbnail() to offer relevant thumbnail sized based on known metadata about the thumbnail - Renamed DLNAResource.addThumbnailResource() to DLNAResource.addImageResource():   - Generalized it to handle any DLNA compliant image profile   - Implemented support for DLNA.ORG_CI flag to indicate if the content is transcoded or not   - Added size and resolution information to the "res" string if it's known - Implemented final "wrapper" methods in DLNAResource for the thumbnail input stream getters to keep some metadata about the thumbnails - Made GenericIcons handle the default folder thumbnail as well - "Encoded" the DLNA media format profile information in the thumbnail URL in DLNAResource.getThumbURL() - Updated Request and RequestV2 to "decode" and use the thumbnail DLNA media format profile information - Implemented support for FullyPlayed in RemoteThumbHandler - Made RemoteThumbHandler send thumbnails in their existing format and size - Did some refactoring in CustomImageReader to better close stream resources - Created UnknownFormatException in CustomImageReader so that it's easier for callers to pick up missing format support - Made FullyPlayed work in memory instead of on disk for improved speed - Made FullyPlayed reset the source input stream before it's returned in case the operation fails - Made GenericIcons cac

src/main/java/net/pms/remote/RemoteWeb.java
@@ -20,10 +20,12 @@ import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.configuration.WebRender;
 import net.pms.dlna.DLNAResource;
+import net.pms.dlna.DLNAThumbnailInputStream;
+import net.pms.dlna.RealFile;
 import net.pms.dlna.RootFolder;
 import net.pms.network.HTTPResource;
 import net.pms.newgui.DbgPacker;
-import net.pms.util.ImagesUtil;
+import net.pms.util.FullyPlayed;
 import net.pms.util.ImagesUtil.ImageFormat;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.lang.StringUtils;
@@ -287,23 +289,25 @@ public class RemoteWeb {
 				LOGGER.debug("media unknown");
 				throw new IOException("Bad id");
 			}
-			InputStream in;
+			DLNAThumbnailInputStream in;
 			if (!configuration.isShowCodeThumbs() && !r.isCodeValid(r)) {
 				// we shouldn't show the thumbs for coded objects
 				// unless the code is entered
 				in = r.getGenericThumbnailInputStream(null);
 			} else {
 				r.checkThumbnail();
-				in = r.getThumbnailInputStream();
+				in = r.fetchThumbnailInputStream();
 			}
-			in = ImagesUtil.convertImage(in, ImageFormat.JPEG);
+			if (r instanceof RealFile && FullyPlayed.isFullyPlayedThumbnail(((RealFile) r).getFile())) {
+				in = FullyPlayed.addFullyPlayedOverlay(in);
+			}
 			Headers hdr = t.getResponseHeaders();
-			hdr.add("Content-Type", HTTPResource.JPEG_TYPEMIME);
+			hdr.add("Content-Type", ImageFormat.PNG.equals(in.getFormat()) ? HTTPResource.PNG_TYPEMIME : HTTPResource.JPEG_TYPEMIME);
 			hdr.add("Accept-Ranges", "bytes");
 			hdr.add("Connection", "keep-alive");
-			t.sendResponseHeaders(200, in.available());
+			t.sendResponseHeaders(200, in.getSize());
 			OutputStream os = t.getResponseBody();
-			LOGGER.trace("input is {} output is {}", in, os);
+			LOGGER.trace("Web thumnail: Input is {} output is {}", in, os);
 			RemoteUtil.dump(in, os);
 		}
 	}

DigitalMediaServer/DigitalMediaServer

src/main/java/net/pms/remote/RemoteWeb.java
@@ -20,10 +20,12 @@ import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.configuration.WebRender;
 import net.pms.dlna.DLNAResource;
+import net.pms.dlna.DLNAThumbnailInputStream;
+import net.pms.dlna.RealFile;
 import net.pms.dlna.RootFolder;
 import net.pms.network.HTTPResource;
 import net.pms.newgui.DbgPacker;
-import net.pms.util.ImagesUtil;
+import net.pms.util.FullyPlayed;
 import net.pms.util.ImagesUtil.ImageFormat;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.lang.StringUtils;
@@ -287,23 +289,25 @@ public class RemoteWeb {
 				LOGGER.debug("media unknown");
 				throw new IOException("Bad id");
 			}
-			InputStream in;
+			DLNAThumbnailInputStream in;
 			if (!configuration.isShowCodeThumbs() && !r.isCodeValid(r)) {
 				// we shouldn't show the thumbs for coded objects
 				// unless the code is entered
 				in = r.getGenericThumbnailInputStream(null);
 			} else {
 				r.checkThumbnail();
-				in = r.getThumbnailInputStream();
+				in = r.fetchThumbnailInputStream();
 			}
-			in = ImagesUtil.convertImage(in, ImageFormat.JPEG);
+			if (r instanceof RealFile && FullyPlayed.isFullyPlayedThumbnail(((RealFile) r).getFile())) {
+				in = FullyPlayed.addFullyPlayedOverlay(in);
+			}
 			Headers hdr = t.getResponseHeaders();
-			hdr.add("Content-Type", HTTPResource.JPEG_TYPEMIME);
+			hdr.add("Content-Type", ImageFormat.PNG.equals(in.getFormat()) ? HTTPResource.PNG_TYPEMIME : HTTPResource.JPEG_TYPEMIME);
 			hdr.add("Accept-Ranges", "bytes");
 			hdr.add("Connection", "keep-alive");
-			t.sendResponseHeaders(200, in.available());
+			t.sendResponseHeaders(200, in.getSize());
 			OutputStream os = t.getResponseBody();
-			LOGGER.trace("input is {} output is {}", in, os);
+			LOGGER.trace("Web thumbnail: Input is {} output is {}", in, os);
 			RemoteUtil.dump(in, os);
 		}
 	}