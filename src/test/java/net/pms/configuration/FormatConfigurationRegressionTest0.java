5a6cd69fa3459155aba668456e9423dde2996e81
Samuel <Sami32@users.noreply.github.com>
2017-08-21 22:56:19 +0200

    Add RLE codecs recognition

--- src/test/java/net/pms/configuration/FormatConfigurationRegressionTest0.java
+++ src/test/java/net/pms/configuration/FormatConfigurationRegressionTest0.java
@@ -766,4 +766,13 @@ public class FormatConfigurationRegressionTest0 {
 
   }
 
+  @Test
+  public void testRLE() throws Throwable {
+    java.lang.String str0 = net.pms.configuration.FormatConfiguration.RLE;
+
+    // Regression assertion (captures the current behavior of the code)
+    org.junit.Assert.assertTrue("'" + str0 + "' != '" + "rle"+ "'", str0.equals("rle"));
+
+  }
+
 }
