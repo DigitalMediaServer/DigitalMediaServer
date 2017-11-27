6c28c52cfc8da7cb0b33acc484cd896f460c3dc7
Nadahar <Nadahar@users.noreply.github.com>
2017-11-27 19:29:25 +0100

    Created WinUtils.getOEMCharset()

--- src/main/java/net/pms/io/WinUtils.java
+++ src/main/java/net/pms/io/WinUtils.java
@@ -28,6 +28,9 @@ import com.sun.jna.platform.win32.WinReg;
 import com.sun.jna.ptr.LongByReference;
 import java.io.File;
 import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.annotation.Nullable;
@@ -216,6 +219,25 @@ public class WinUtils extends BasicSystemUtils {
 	 */
 	public static int getConsoleOutputCP() {
 		return Kernel32.INSTANCE.GetConsoleOutputCP();
+	}
+
+	/**
+	 * @return The result of {@link #getOEMCP()} converted to a {@link Charset}
+	 *         or {@code null} if it couldn't be converted.
+	 */
+	public static Charset getOEMCharset() {
+		int codepage = Kernel32.INSTANCE.GetOEMCP();
+		Charset result = null;
+		String[] aliases = {"cp" + codepage, "MS" + codepage};
+		for (String alias : aliases) {
+			try {
+				result = Charset.forName(alias);
+				break;
+			} catch (IllegalCharsetNameException | UnsupportedCharsetException e) {
+				result = null;
+			}
+		}
+		return result;
 	}
 
 	private static String charString2String(CharBuffer buf) {
