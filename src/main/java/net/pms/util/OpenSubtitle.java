valib/UniversalMediaServer

- Refactored/rewrote ImdbUtil:   - Switched to using java.nio instead of java.io   - Removed cleanName()   - Replaced extractImdb() with extractImdbId() which can also scan relevant .nfo files for IMDB ID   - Fixed NPE in ensureTT()   - Created removeTT()   - Replaced extract() with extractFromFileName()   - Created extractImdbIdFromNfo() - Created new methods FileUtil.detectCharset()

src/main/java/net/pms/util/OpenSubtitle.java
@@ -224,7 +224,7 @@ public class OpenSubtitle {
 
 	public static String getHash(File f) throws IOException {
 		LOGGER.debug("get hash of " + f);
-		String hash = ImdbUtil.extractOSHash(f);
+		String hash = ImdbUtil.extractOSHash(f.toPath());
 		if (!StringUtils.isEmpty(hash)) {
 			return hash;
 		}
@@ -238,7 +238,7 @@ public class OpenSubtitle {
 	public static Map<String, Object> findSubs(File f, RendererConfiguration r) throws IOException {
 		Map<String, Object> res = findSubs(getHash(f), f.length(), null, null, r);
 		if (res.isEmpty()) { // no good on hash! try imdb
-			String imdb = ImdbUtil.extractImdb(f);
+			String imdb = ImdbUtil.extractImdbId(f.toPath(), false);
 			if (StringUtils.isEmpty(imdb)) {
 				imdb = fetchImdbId(f);
 			}
@@ -331,7 +331,7 @@ public class OpenSubtitle {
 	public static String[] getInfo(File f, String formattedName, RendererConfiguration r) throws IOException {
 		String[] res = getInfo(getHash(f), f.length(), null, null, r);
 		if (res == null || res.length == 0) { // no good on hash! try imdb
-			String imdb = ImdbUtil.extractImdb(f);
+			String imdb = ImdbUtil.extractImdbId(f.toPath(), false);
 			if (StringUtil.hasValue(imdb)) {
 				res = getInfo(null, 0, imdb, null, r);
 			}

DigitalMediaServer/DigitalMediaServer

src/main/java/net/pms/util/OpenSubtitle.java
@@ -249,7 +249,7 @@ public class OpenSubtitle {
 
 	public static String getHash(File file) throws IOException {
 		LOGGER.trace("Getting OpenSubtitles hash for \"{}\"", file);
-		String hash = ImdbUtil.extractOSHash(file);
+		String hash = ImdbUtil.extractOSHash(file.toPath());
 		if (!StringUtils.isEmpty(hash)) {
 			return hash;
 		}
@@ -263,7 +263,7 @@ public class OpenSubtitle {
 	public static Map<String, Object> findSubs(File file, RendererConfiguration renderer) throws IOException {
 		Map<String, Object> result = findSubs(getHash(file), file.length(), null, null, renderer);
 		if (result.isEmpty()) { // no good on hash! try imdb
-			String imdb = ImdbUtil.extractImdb(file);
+			String imdb = ImdbUtil.extractImdbId(file.toPath(), false);
 			if (StringUtils.isEmpty(imdb)) {
 				imdb = fetchImdbId(file);
 			}
@@ -396,7 +396,7 @@ public class OpenSubtitle {
 	public static String[] getInfo(File file, String formattedName, RendererConfiguration renderer) throws IOException {
 		String[] res = getInfo(getHash(file), file.length(), null, null, renderer);
 		if (res == null || res.length == 0) { // no good on hash! try imdb
-			String imdb = ImdbUtil.extractImdb(file);
+			String imdb = ImdbUtil.extractImdbId(file.toPath(), false);
 			if (isNotBlank(imdb)) {
 				res = getInfo(null, 0, imdb, null, renderer);
 			}