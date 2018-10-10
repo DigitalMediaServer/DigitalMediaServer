d0dd8d7a4ece09a7c322c680238569120fc646fe
Nadahar <Nadahar@users.noreply.github.com>
2018-10-10 16:27:03 +0200

    Created a new logic for resolving paths found in project.properties:  - Created PropertiesUtil.resolvePropertiesPathEntry()  - Added a new property for the default "profile folder name"  - Updated PlatformProgramPaths to use the new logic  - Updated LoggingConfig to use the new logic

--- src/main/java/net/pms/util/PropertiesUtil.java
+++ src/main/java/net/pms/util/PropertiesUtil.java
@@ -18,27 +18,44 @@
  */
 package net.pms.util;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.pms.util.FilePermissions.FileFlag;
 
 public class PropertiesUtil {
-	/**
-	 * Logs messages to all different channels.
-	 */
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesUtil.class);
+	private static final String DEFAULT_PROFILE_FOLDER_NAME;
 
 	/**
-	 * General properties for the DMS project.
+	 * General properties for the project.
 	 */
-	private static final PmsProperties projectProperties = new PmsProperties();
+	private static final PmsProperties PROJECT_PROPERTIES = new PmsProperties();
 
 	static {
 		try {
 			// Read project properties resource file.
-			projectProperties.loadFromResourceFile("/resources/project.properties");
+			PROJECT_PROPERTIES.loadFromResourceFile("/resources/project.properties");
+			DEFAULT_PROFILE_FOLDER_NAME = PROJECT_PROPERTIES.get("project.profile.folder.name");
 		} catch (IOException e) {
-			LOGGER.error("Could not load project.properties");
+			throw new Error("Could not load \"/resources/project.properties\"", e);
 		}
 	}
 
@@ -54,6 +71,149 @@ public class PropertiesUtil {
 	 * @return The properties object.
 	 */
 	public static PmsProperties getProjectProperties() {
-		return projectProperties;
+		return PROJECT_PROPERTIES;
+	}
+
+	/**
+	 * @return The default profile folder name (e.g "DigitalMediaServer"), not
+	 *         the complete profile path.
+	 */
+	public static String getDefaultProfileFolderName() {
+		return DEFAULT_PROFILE_FOLDER_NAME;
+	}
+
+	/**
+	 * Resolves a file or folder path specified in the project properties. The
+	 * property value can have multiple, comma separated entries. The first
+	 * non-blank, valid entry that satisifies the specified {@link FileFlag}s
+	 * will be returned. If none can be resolved, {@code defaultValue} will be
+	 * returned.
+	 *
+	 * @param key the property key to use.
+	 * @param baseFolder the folder from which to resolve the path or
+	 *            {@code null} to use the current working folder.
+	 * @param defaultValue the default {@link Path} to return if none cane be
+	 *            resolved.
+	 * @param checkFlags the {@link Set} of {@link FileFlag} that is required to
+	 *            be true for the file or folder to resolve.
+	 * @param createFolder if {@code true} and {@link FileFlag#FOLDER} is
+	 *            specified in {@code checkFlags} and no folder can be resolved,
+	 *            an attempt to create the folder will be made.
+	 * @param options the {@link LinkOption}s to use when resolving paths.
+	 * @return The resolved or default {@link Path}.
+	 */
+	@Nullable
+	public static Path resolvePropertiesPathEntry(
+		@Nonnull String key,
+		@Nullable Path baseFolder,
+		@Nullable Path defaultValue,
+		@Nullable Set<FileFlag> checkFlags,
+		boolean createFolder,
+		@Nullable LinkOption... options
+	) {
+		return resolvePropertiesPathEntry(key, baseFolder, defaultValue, checkFlags, null, createFolder, options);
+	}
+
+	/**
+	 * Resolves a file or folder path specified in the project properties. The
+	 * property value can have multiple, comma separated entries. The first
+	 * non-blank, valid entry that satisfies the specified {@link FileFlag}s
+	 * will be returned. If none can be resolved, {@code defaultValue} will be
+	 * returned.
+	 *
+	 * @param key the property key to use.
+	 * @param baseFolder the folder from which to resolve the path or
+	 *            {@code null} to use the current working folder.
+	 * @param defaultValue the default {@link Path} to return if none cane be
+	 *            resolved.
+	 * @param checkFlags the {@link Set} of {@link FileFlag} that is required to
+	 *            be true for the file or folder to resolve.
+	 * @param replaceAlls a {@link Map} of source and target regular expressions
+	 *            to be replaced in the property value.
+	 * @param createFolder if {@code true} and {@link FileFlag#FOLDER} is
+	 *            specified in {@code checkFlags} and no folder can be resolved,
+	 *            an attempt to create the folder will be made.
+	 * @param options the {@link LinkOption}s to use when resolving paths.
+	 * @return The resolved or default {@link Path}.
+	 */
+	@Nullable
+	public static Path resolvePropertiesPathEntry(
+		@Nonnull String key,
+		@Nullable Path baseFolder,
+		@Nullable Path defaultValue,
+		@Nullable Set<FileFlag> checkFlags,
+		@Nullable Map<String, String> replaceAlls,
+		boolean createFolder,
+		@Nullable LinkOption... options
+	) {
+		if (baseFolder == null) {
+			baseFolder = Paths.get("");
+		}
+		String value = PROJECT_PROPERTIES.get(key);
+		if (replaceAlls != null) {
+			for (Entry<String, String> replaceAll : replaceAlls.entrySet()) {
+				value = value.replaceAll(replaceAll.getKey(), replaceAll.getValue());
+			}
+		}
+		List<String> pathList = new ArrayList<>(Arrays.asList(value.split("\\s*,\\s*")));
+		for (Iterator<String> iterator = pathList.iterator(); iterator.hasNext();) {
+			String pathEntry = iterator.next();
+			try {
+				Path path = Paths.get(pathEntry.trim());
+				if (!path.isAbsolute()) {
+					path = baseFolder.resolve(path);
+				}
+				if (checkFlags == null || checkFlags.isEmpty()) {
+					return path;
+				}
+				if (new FilePermissions(path, options).hasFlags(checkFlags)) {
+					return path;
+				}
+			} catch (InvalidPathException e) {
+				LOGGER.debug("Invalid properties path specified for \"{}\": \"{}\"", key, pathEntry);
+				iterator.remove();
+			} catch (FileNotFoundException e) {
+				// Nothing to do, keep looking
+			}
+		}
+
+		// Unable to resolve, create folder if appropriate
+
+		if (createFolder && checkFlags != null && checkFlags.contains(FileFlag.FOLDER)) {
+			for (String pathEntry : pathList) {
+				if (isNotBlank(pathEntry)) {
+					Path path = Paths.get(pathEntry.trim());
+					if (!path.isAbsolute()) {
+						path = baseFolder.resolve(path);
+					}
+					if (!Files.exists(path, options)) {
+						try {
+							Files.createDirectories(path);
+							if (new FilePermissions(path, options).hasFlags(checkFlags)) {
+								return path;
+							}
+							// The folder was created but without the required permissions, try to delete it again.
+							try {
+								Files.delete(path);
+							} catch (IOException e) {
+								LOGGER.error(
+									"Folder \"{}\" was created but didn't satisfy the requirements ({}). " +
+									"The attempt to delete unsatisfactory folder failed with: {}",
+									path,
+									StringUtil.createReadableCombinedString(checkFlags),
+									e.getMessage()
+								);
+								LOGGER.trace("", e);
+							}
+						} catch (IOException e) {
+							LOGGER.error("Could not create folder \"{}\": {}", path, e.getMessage());
+							LOGGER.trace("", e);
+						}
+					}
+				}
+			}
+		}
+
+		return defaultValue;
 	}
 }
