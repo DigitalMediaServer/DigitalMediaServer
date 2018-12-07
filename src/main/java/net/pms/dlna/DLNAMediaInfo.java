59a9962ef89bc61e4afed4e0bd9f49902945e4cf
Sami32 <Sami32@users.noreply.github.com>
2018-12-07 01:02:30 +0100

    Again more thumbnail enhancement
    Signed-off-by: Sami32 <Sami32@users.noreply.github.com>

--- src/main/java/net/pms/dlna/DLNAMediaInfo.java
+++ src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -608,10 +608,18 @@ public class DLNAMediaInfo implements Cloneable {
 			args.add("-");
 		}
 
+		args.add("-map");
+		args.add("0:V");
 		args.add("-an");
 		args.add("-dn");
 		args.add("-sn");
 		if (generateThumbnail) {
+			args.add("-c:v");
+			args.add("mjpeg");
+			args.add("-pix_fmt");
+			args.add("yuvj420p");
+			args.add("-q:v");
+			args.add("2");
 			args.add("-vf");
 			args.add("select='eq(pict_type,PICT_TYPE_I)',scale='if(gte(iw,ih),320,trunc(iw*sar*oh/ih))':'if(gte(iw,ih),trunc(ih*ow/iw/sar),320)'");
 			args.add("-vsync");
